633f34986d43f8d89916e57a008e01a73287f46a
Jon Earley <jonathan.a.earley@gmail.com>
2021-02-16 11:05:29 -0500

    Add server side rendering for web components.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,18 @@
+const stencil = require("@umich-lib/components/hydrate");
+
 module.exports = function (eleventyConfig) {
-  // Copy everything in static to _site
   eleventyConfig.addPassthroughCopy("static");
+
+  // Server side rendering for @umich-lib/components.
+  eleventyConfig.addTransform("ssr", async (content, outputPath) => {
+    if (outputPath.endsWith(".html")) {
+      try {
+        const { html } = await stencil.renderToString(content);
+        return html;
+      } catch (error) {
+        return error;
+      }
+    }
+    return content;
+  });
 };
