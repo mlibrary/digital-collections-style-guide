8dfc0668ef5eb33e2bfc859f64b3c0f7622dd1b5
Roger Espinosa <roger@umich.edu>
2021-12-14 15:44:58 -0500

    shuffle all the things so it can be checked out on quod

--- js/image/base.js
+++ js/image/base.js
@@ -1,15 +1,4 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-  const usingIdentifiers = document.querySelectorAll('[identifier]').length > 0;
-  document.body.addEventListener("click", (event) => {
-    const target = event.target.closest('a');
-    if (usingIdentifiers && target && target.getAttribute("href")[0] != '#') {
-      if (target.dataset.available != "true") {
-        event.preventDefault();
-        alert("This link is not available.");
-        return;
-      }
-    }
-  });
 
   if ( location.hostname.indexOf('.netlify.app') < 0 ) {
     // login trigger
@@ -117,25 +106,4 @@ window.addEventListener('DOMContentLoaded', (event) => {
       }
     })
   })
-
-  if ( usingIdentifiers ) {
-    const button = document.createElement("button");
-    button.classList.add("button", "button--small");
-    button.innerHTML = '<span class="material-icons">pets</span>';
-    button.style.position = "fixed";
-    button.style.bottom = "1rem";
-    button.style.right = "1rem";
-    document.body.appendChild(button);
-
-    button.addEventListener("click", (event) => {
-      document.documentElement.dataset.previewLinks =
-        document.documentElement.dataset.previewLinks == "true" ? false : true;
-
-      if (document.documentElement.dataset.previewLinks == "true") {
-        button.style.filter = "invert(1)";
-      } else {
-        button.style.filter = null;
-      }
-    });
-  }
 });
--- samples/xsl/i/image/uplift/qui/qui.results-table.xsl
+++
@@ -1,4 +0,0 @@
-<xsl:stylesheet version="1.0" xmlns="http://www.w3.org/1999/xhtml" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:dlxs="http://dlxs.org" xmlns:qui="http://dlxs.org/quombat/ui">
-  <xsl:output method="xml" version="1.0" encoding="utf-8" indent="yes" />
-
-</xsl:stylesheet>
