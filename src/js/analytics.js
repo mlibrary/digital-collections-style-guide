5ed16854c1981cfa4655656de63aad82b3156e94
Roger Espinosa <roger@umich.edu>
2024-10-02 11:03:35 -0400

    add clarity analytics

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -5,6 +5,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MN98W2F');
 
+// clarity
+(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "6cykzxa2og");
+
 const _visited = {};
 const trackPageView = function (options) {
 }
